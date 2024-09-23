400596fecc1b4338266ba302b16caeb73d7dfaef
Thormes Filgueira Leite Pereira <thormes@aluno.unifapce.edu.br>
2024-09-23 19:14:08 -0300

    Salvar database no repositório

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/', function (Request $request){
+    return 'Teste';
+});
